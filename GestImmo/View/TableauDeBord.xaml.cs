8a1b2e067e6f0d12c50026550bbbb29a6b82fb3b
guales <guilhem.ales@gmail.com>
2017-07-30 10:59:02 +0200

    Ajoutez des fichiers projet.

---
+++ GestImmo/View/TableauDeBord.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GestImmo.View
+{
+    /// <summary>
+    /// Logique d'interaction pour TableauDeBord.xaml
+    /// </summary>
+    public partial class TableauDeBord : Page
+    {
+        public TableauDeBord()
+        {
+            InitializeComponent();
+        }
+
+
+        protected void GoTo_RecapitulatifPaiments(object sender, RoutedEventArgs e)
+        {
+            // TODO
+        }
+
+        protected void GoTo_Frais(object sender, RoutedEventArgs e)
+        {
+            // TODO
+        }
+
+        protected void GoTo_Logement(object sender, RoutedEventArgs e)
+        {
+            // TODO
+        }
+
+        protected void GoTo_Remboursements(object sender, RoutedEventArgs e)
+        {
+            // TODO
+        }
+
+        protected void GoTo_SynthèseFinanciere(object sender, RoutedEventArgs e)
+        {
+            // TODO
+        }
+    }
+}
